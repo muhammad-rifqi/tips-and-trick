4099244ccb19bd221d5a2e7769bd6051d5e0a6b5
Muhammad Rifqi <muhammad45rifki@gmail.com>
2023-08-25 16:05:57 +0700

    Update auth_creation.php

--- get-post-guzzle/auth_creation.php
+++ get-post-guzzle/auth_creation.php
@@ -15,17 +15,17 @@ $data = array(
         "description"=> "productive Snap",
         "partnerReferenceNo"=> "20230213000126",
         "additionalInfo" => array(
-            "notifUrl" => "https://arneva.co.id/fello_snap/v1/callback"
+            "notifUrl" => "https://arneva.co.id/v1/callback"
         )
  );
- $url = "https://service.synxchro.co.id/fello_snap/v1/utilities/signature/service";
+ $url = "url";
  $response = $client->post($url, [
      'headers' => [
      'x-timestamp'=>'2023-02-13T00:00:00+07',
-     'x-client-secret' => 'JhR0nSqHxhWe+MkQuzGbr5cwyc3iPBzhpUuPZLvPVoRbbI8ve36Zcz81UL+5Z//Sit7Mzu+qKs5mK29FhzcdhQ==',
+     'x-client-secret' => '',
      'httpmethod'=>'post',
-     'endpoinurl'=>'https://arneva.co.id/fello_snap/v1/registration-account-creation',
-     'accesstoken' => '06feca9225a1e3fb838e69892ef80487a47e52d3',
+     'endpoinurl'=>'https://arneva.co.id/v1/registration-account-creation',
+     'accesstoken' => '',
      'content-type'=>'application/json'
     ],
      'body'    => json_encode($data)
